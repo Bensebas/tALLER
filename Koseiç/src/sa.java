39520bdf5653d9e283c326fd894d7290f5835294
USUARIO <USUARIO@192.168.200.7>
2021-07-26 17:43:04 -0500

    mensaje

---
+++ Koseiç/src/sa.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class sa {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		 int x = 0;
+		 Scanner sc = new Scanner(System.in);
+		 int age = sc.nextInt();
+	        
+	        while (x < 4)
+	        {
+	            System.out.println("*" + x);
+	 
+	            
+	            x++;
+	        }
+	}
+
+}
