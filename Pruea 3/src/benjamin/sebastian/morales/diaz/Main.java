950dc257900c1c52ad0e6919f1ec290e7038ef74
USUARIO <USUARIO@192.168.200.7>
2021-07-26 20:01:43 -0500

    error

---
+++ Pruea 3/src/benjamin/sebastian/morales/diaz/Main.java
@@ -0,0 +1,19 @@
+package benjamin.sebastian.morales.diaz;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner Lector = new Scanner (System.in);
+		int n = 0 ;
+		n = Lector.nextInt();
+		do {System.out.println("Ingrese un número");}
+		while (n>0) {}
+
+	}
+
+}
