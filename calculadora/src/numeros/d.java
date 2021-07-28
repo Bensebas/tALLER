d1d6e00a8948e3de78ea6ce27d8829fa46c93a8a
USUARIO <USUARIO@192.168.200.7>
2021-07-28 17:52:38 -0500

    prueba

---
+++ calculadora/src/numeros/d.java
@@ -0,0 +1,17 @@
+package numeros;
+
+import java.util.Scanner;
+
+public class d {
+
+	public static void main(String[] args) {
+
+		Scanner Lector = new Scanner (System.in);
+		int n = 0 ;
+		n = Lector.nextInt();
+		while (n>0) {System.out.println("Ingrese un número");}
+
+
+	}
+
+}
