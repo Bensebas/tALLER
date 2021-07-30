af57f80184196bbf1c263e1ac67799294ffc5f4b
USUARIO <USUARIO@192.168.200.7>
2021-07-30 17:52:49 -0500

    listo1

---
+++ Tarea/src/princ.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class princ {
+
+	public static void main(String[] args) {
+		Scanner primero = new Scanner (System.in);
+		int a=0;
+		float b=0;
+		int c=0;
+		int d= 0;
+		System.out.println("Ingrese un número +");
+		int n = primero.nextInt();
+		while (n!=0);{
+		
+		System.out.println("Ingrese un número +");
+		n = primero.nextInt();
+		 a=a+n;
+		 b=a/d;
+		 c=c+1;}
+		 
+		System.out.println("La suma de los números ingresados es : "+ a);
+		System.out.println("El promedioi de los númerros ingresados es : " + b);
+		System.out.println("La cantidad de números ingresados es " + c);
+		
+		
+		
+		
+
+	}
+
+}
