68e59b8bf48e12bfdcab31471e8e90e25d0e9785
USUARIO <USUARIO@192.168.200.7>
2021-07-30 17:53:05 -0500

    ds

--- Tarea 2/src/Main.java
+++ Tarea 2/src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner segundo = new Scanner (System.in);
+		
+		int a = 0 ;
+		int b = 0 ;
+		int c = 0 ;
+		float d = 0 ;
+		
+		do { }while();
+		
+		System.out.println("La suma de los números ingresados es : "+ a);
+		System.out.println("El promedioi de los númerros ingresados es : " + b);
 
 	}
 
