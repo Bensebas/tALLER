ec70fa2b203439697c5be12761fcd9f253232bd2
USUARIO <USUARIO@192.168.200.7>
2021-08-11 20:26:34 -0500

    mensaje

---
+++ proyect/src/ejercicios/metodos/Impresora.java
@@ -0,0 +1,27 @@
+//1. declaración del paquete
+package ejercicios.metodos;
+//2.declaración de importaciones
+//3. Comentarios
+//4. Declaración de la clase
+public class Impresora {
+	//5. constantes
+	//6. Variables
+	public String color;
+	
+	public Impresora() {}
+	public void imprimit(String datosAImprimir, int letra) {
+		System.out.println("Logo UCE");
+		System.out.println("***********");
+		System.out.println(datosAImprimir);
+		System.out.println("***********");
+		System.out.println("Pie de firma");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ proyect/src/ejercicios/metodos/Matricula.java
@@ -0,0 +1,14 @@
+package ejercicios.metodos;
+
+public class Matricula {
+
+	public static void main(String[] args) {
+		
+		
+		Impresora impresion1 = new Impresora();
+		int nemroImpresion = impresion1.imprimir("Programación 1 :20 - Investigación 17", 8);
+		System.out.println("Se imprimio la matricula con número de impresióm; "+ numeroImpresion);
+
+	}
+
+}
