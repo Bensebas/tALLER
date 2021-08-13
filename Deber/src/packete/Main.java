c1c899d1374bd70425d7b000ebd464461a4ebcb9
USUARIO <USUARIO@192.168.200.7>
2021-08-13 17:51:34 -0500

    messague

---
+++ Deber/src/packete/Main.java
@@ -0,0 +1,13 @@
+package packete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Name name1 = new Name();
+		name1.informacion();
+
+	}
+
+}
---
+++ Deber/src/packete/Name.java
@@ -0,0 +1,16 @@
+package packete;
+
+public class Name {
+	
+    public String Jose;
+	int age = 17;
+	int ID = 1719194332;
+
+	public void informacion() {
+		
+		System.out.println(Jose);
+		System.out.println(age);
+		System.out.println(ID);
+
+	}
+}
