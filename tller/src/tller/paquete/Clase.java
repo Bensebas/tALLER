fe40f79e45f44b4a885aad9551ca6e22d334f132
USUARIO <USUARIO@192.168.200.7>
2021-09-07 20:07:14 -0500

    comm

---
+++ tller/src/tller/paquete/Clase.java
@@ -0,0 +1,10 @@
+package tller.paquete;
+
+public class Clase {
+	
+	private String nombre ;
+	private int edad ;
+	public static String provincia ="Pichincha";
+	public static String nombr = "Jose";
+
+}
---
+++ tller/src/tller/paquete/Main.java
@@ -0,0 +1,12 @@
+package tller.paquete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Valor dek atributo primero "+ Clase.provincia);
+		System.out.println("Valor dek atributo primero "+ Clase.nombr);
+
+	}
+
+}
