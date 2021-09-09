a5abb130b82bcf45133893b1390032c111f3a5f9
USUARIO <USUARIO@192.168.200.7>
2021-09-08 21:52:43 -0500

    COMMIT

--- Estudiante/src/paquete/estudiante/Main.java
+++ Estudiante/src/paquete/estudiante/Main.java
@@ -5,55 +5,66 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("******************************************");
-		System.out.println("1.       Ingresar Estudiante");
-		System.out.println("2.       Buscar Estudiante");
-		System.out.println("3.       SALIR");
-		System.out.println("******************************************");
-		
+
+		String cedula = null;
+
+		Metodos estudiante1 = new Metodos();
+		estudiante1.setCedulaVerdad(cedula);
+		Metodos estudiante2 = new Metodos();
+		estudiante2.setCedulaVerdad(cedula);
+		Metodos estudiante3 = new Metodos();
+		estudiante3.setCedulaVerdad(cedula);
+		Metodos estudiante4 = new Metodos();
+		estudiante4.setCedulaVerdad(cedula);
+		Metodos estudiante5 = new Metodos();
+		estudiante5.setCedulaVerdad(cedula);
+
 		Scanner lector = new Scanner(System.in);
-		int Estudiante [ ] = new int [5];
-		int cedula = 0 ;
-		int seleccion ;
-		int cosa = 0 ;
-		
-		
-		
+		Metodos Estudiante[] = new Metodos[5];
+		Estudiante[0] = estudiante1;
+		Estudiante[1] = estudiante2;
+		Estudiante[2] = estudiante3;
+		Estudiante[3] = estudiante4;
+		Estudiante[4] = estudiante5;
+
+		int seleccion;
+
 		do {
-			seleccion = lector.nextInt();
-			if (seleccion >1&& seleccion<4 ) {
-				cosa = 1;
-		}else {
-			System.out.println("Ingrese una opcion");
-			}
-		} while(cosa == 0);
-		
-		if (seleccion == 1 ) {
-			System.out.println(" Ingresar No de cedula");
-			cedula = lector.nextInt();
-			Estudiante[0]= cedula;
 			System.out.println("******************************************");
 			System.out.println("1.       Ingresar Estudiante");
 			System.out.println("2.       Buscar Estudiante");
 			System.out.println("3.       SALIR");
 			System.out.println("******************************************");
-			
-	}else if(seleccion == 2){
-		cedula = lector.nextInt();
-		System.out.println("******************************************");
-		System.out.println("1.       Ingresar Estudiante");
-		System.out.println("2.       Buscar Estudiante");
-		System.out.println("3.       SALIR");
-		System.out.println("******************************************");
 
-	}else if(seleccion == 3){
-		System.out.println("Salir");
-		
+			seleccion = lector.nextInt();
+			cedula = lector.nextLine();
+			if (seleccion == 1) {
+				for (int i = 0; i < 5; i++) {
+					Metodos e = Estudiante[i];
+				System.out.println("Ingrese la cedula");
+				cedula = lector.nextLine();
+				}
 
+			} else if (seleccion == 2) {
+				System.out.println("Ingrese la cedula");
+				for (int i = 0; i < 5; i++) {
+					Metodos e = Estudiante[i];
+					String no1 = cedula;
+					String no2 = lector.nextLine();
+					boolean igual = no1.equals(no2);
+					if(igual == true) {
+						System.out.println("SI");
+						
+					}else {
+						System.out.println("NO");
+					}
+				}
+
+			} else if (seleccion == 3) {
+				System.out.println("Salio");
+				seleccion = lector.nextInt();
+			}
+
+		} while (seleccion > 0 && seleccion < 4);
 	}
-		
-
-	}
-
 }
---
+++ Estudiante/src/paquete/estudiante/Metodos.java
@@ -0,0 +1,16 @@
+package paquete.estudiante;
+
+public class Metodos extends Main {
+	
+	public String cedulaVerdad ;
+
+	public String getCedulaVerdad() {
+		return cedulaVerdad;
+	}
+
+	public void setCedulaVerdad(String cedulaVerdad) {
+		this.cedulaVerdad = cedulaVerdad;
+	}
+	
+
+}
