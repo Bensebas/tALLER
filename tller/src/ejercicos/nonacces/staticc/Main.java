67862c12656711250b5bc40c6036a8399e247d7d
USUARIO <USUARIO@192.168.200.7>
2021-09-08 19:59:11 -0500

    comm

---
+++ tller/src/ejercicos/nonacces/staticc/Main.java
@@ -0,0 +1,16 @@
+package ejercicos.nonacces.staticc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Paciente paciente1 = new Paciente ();
+		paciente1.setNombre("Juanito");
+		paciente1.setEdad(25);
+		
+		paciente1.registrar(paciente1.getNombre());
+		paciente1.consultarNacionalidadStatica();
+
+	}
+
+}
