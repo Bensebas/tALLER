f0e7c0f0d0f63f59b23c07df5234f79b55c61383
USUARIO <USUARIO@192.168.200.7>
2021-09-15 20:01:48 -0500

    mit

---
+++ maven_proyecto_bm/scr/main/java/ejercicios/clases/Maindoll.java
@@ -0,0 +1,54 @@
+package ejercicios.clases;
+
+public class Maindoll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte datoByte = 2 ;
+		Byte datoByteWrapper = 3 ;
+		System.out.println("primitivo :" + datoByte);
+		System.out.println("Wrapper"+ datoByteWrapper);
+		
+		short datoShort = 1 ;
+		Short datoShotoWrapper = 1 ;
+		System.out.println("primitivo :" + datoShort);
+		System.out.println("Wrapper"+ datoShotoWrapper);
+		
+		int edad = 45 ;
+		Integer edadWrapper = 48;
+		System.out.println("primitivo :" + edad);
+		System.out.println("Wrapper"+ edadWrapper);
+		
+		long valorGrande = 123456L ;
+		Long valorGrandeWrapper = 67436789l;
+		System.out.println("primitivo :" + valorGrande );
+		System.out.println("Wrapper"+ valorGrandeWrapper);
+		
+		char caracter = 2 ;
+		Character CaracterWrapper = 7 ;
+		System.out.println("primitivo :" + caracter );
+		System.out.println("Wrapper"+ CaracterWrapper);
+		
+		boolean vof = true ;
+		Boolean vofWrapper = false ;
+		System.out.println("primitivo :" + vof );
+		System.out.println("Wrapper"+ vofWrapper);
+		
+		float floti = 234 ;
+		Float flotiWrapper = 234F ;
+		System.out.println("primitivo :" + floti);
+		System.out.println("Wrapper"+ flotiWrapper);
+		
+		double dos = 76556356;
+		Double doubleWrapper = 6578685D;
+		System.out.println("primitivo :" + dos);
+		System.out.println("Wrapper"+ doubleWrapper);
+		
+		
+		
+		
+		
+	}
+
+}
